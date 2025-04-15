1fd2247a5afb195057ceebc6d88d6da819996a0d
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-04-15 14:14:26 +0200

    programa feito apenas retificado pequenos erro como cconverter um arquvo mp3 para ogg atraves do conversor ffmpeg

--- sfml.cpp
+++ sfml.cpp
@@ -1,9 +1,23 @@
-#include <SFML/audio.hpp>
-#include <ioostream>
+#include <SFML/Audio.hpp>
+#include <iostream>
 
 using namespace std;
 
 int main(){
+    sf::Music plutonio;
+
+    if(!plutonio.openFromFile("plutonio.ogg")){
+        cout<<"Erro ao carregar o arquivo de audio!\n";
+        return  -1;
+    }
+
+    plutonio.play();
+    cout<<"Tocando muusica...";
+
+    //Espera o audio terminar
+    while(plutonio.getStatus()==sf::SoundSource::Playing){
+        sf::sleep(sf::milliseconds(100));
+    }
 
     return 0;
 }
